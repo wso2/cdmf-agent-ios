5421fc686317d9a86de917e66c52e09bd98715a6
Lakshitha Harshan <harshan@wso2.com>
2014-04-29 15:07:57 +0530

    Added unit tests for Settings

---
+++ WSO2 AgentTests/ApiResponseTest.m
@@ -0,0 +1,34 @@
+//
+//  ApiResponseTest.m
+//  agent
+//
+//  Created by Dilshan on 4/25/14.
+//  Copyright (c) 2014 WSO2. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ApiResponseTest : SenTestCase
+
+@end
+
+@implementation ApiResponseTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
